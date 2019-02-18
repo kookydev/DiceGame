72c23c648ea8d74dd14d609cbb2959589965322a
Code Nation 19 <codenation@CN-19.lan>
2019-02-18 17:33:50 +0000

    still not working!!

---
+++ script1player.js
@@ -0,0 +1,82 @@
+let playButton = document.getElementById("play");
+let rollDiceButton = document.getElementById("roll");
+let nextButton = document.getElementById("next");
+let submitButton = document.getElementById("submit");
+let instructions = document.getElementById("instructions");
+let message = document.getElementById("message-box-inner");
+let scoreNumber = document.getElementById("score-number");
+
+
+let roll = 0;
+let score = 0;
+let face = [
+  "./img/dice1.png",
+  "./img/dice2.png",
+  "./img/dice3.png",
+  "./img/dice4.png",
+  "./img/dice5.png",
+  "./img/dice6.png"
+];
+
+
+
+const playGame = () => {
+  instructions.style.display = "none";
+  playButton.style.display = "none";
+  message.innerText = `Click to roll that die!`;
+  rollDiceButton.style.display = "inline";
+  rollDiceButton.addEventListener("click", () => {
+    rollDiceButton.style.display = "none";
+    message.innerText = "";
+    rollDice();
+})}
+
+function rollDice() {
+  let img = document.getElementById("dice-face");
+  let result = Math.floor(Math.random() * 5);
+  roll = result + 1;
+  img.src = `${face[result]}`;
+  img.alt = `${roll}`;
+  score += roll;
+  scoreNumber.innerText = score;
+  rollOutcome();
+}
+
+function rollOutcome() {
+  console.log(`score is ${score}`);
+  if (roll == 1) {
+    message.innerText = `Oh man! It's a 1... \nGame Over - you're BUST!\n Click to play again.`;
+    playButton.style.display = "inline";
+    rollDiceButton.style.display = "none";
+    playButton.addEventListener("click", () => {
+      playButton.style.display = "none";
+      score = 0;
+      scoreNumber.innerText = score;
+      playGame();
+    });
+
+  } else {
+    if (score >=20) {`YOU WIN!!\nClick below to play again`;
+      playButton.style.display = "inline";
+      rollDiceButton.style.display = "none";
+      playButton.addEventListener("click", () => {
+        playButton.style.display = "none";
+        score = 0;
+        playGame();
+      });
+    } 
+    else {
+      message.innerText = `Keep going, your score is ${score} - click to roll again`;
+      rollDiceButton.style.display = "inline";
+      rollDiceButton.addEventListener("click", () => {
+        rollDiceButton.style.display = "none";
+        rollDice();
+      });
+    }
+  }
+}
+
+playButton.addEventListener("click", function() {
+  playGame();
+});
+
